81277517dd17308867e802d608464ffebff735bd
Brannua <374453156@qq.com>
2020-06-07 22:35:02 +0800

    commit: 基本使用方式

---
+++ src/components/baseUse/index.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+// import JSXBaseDemo from './JSXBaseDemo';
+// import ConditionDemo from './ConditionDemo';
+// import ListDemo from './ListDemo';
+// import EventDemo from './EventDemo';
+import FormDemo from './FormDemo';
+
+function BaseUseDemo(props) {
+  return (
+    <Fragment>
+      {/* <JSXBaseDemo /> */}
+      {/* <ConditionDemo /> */}
+      {/* <ListDemo /> */}
+      {/* <EventDemo /> */}
+      <FormDemo />
+    </Fragment>
+  )
+}
+
+export default BaseUseDemo;
